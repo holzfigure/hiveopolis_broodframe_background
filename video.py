30723d898a6ca2eeaaf0f654cbf4203c2a95aa24
marchanti <martin.stefanec@edu.uni-graz.at>
2019-09-04 17:31:28 +0200

    Bug clean up

--- video.py
+++ video.py
@@ -12,7 +12,8 @@ import cv2 as cv
 import glob
 import sys
 
-if len(sys.argv) == 0 or len(sys.argv) > 2:
+print(sys.argv)
+if len(sys.argv) == 0 or len(sys.argv) > 5:
     print("No image name given or to many, only one allowed")
     sys.exit()
 
@@ -21,12 +22,13 @@ if os.path.isdir(str(sys.argv[1])) != True:
     sys.exit()
 
 img_folder = str(sys.argv[1]) + "/*.jpg"
-video_file = str(sys.argv[1]) + ".avi"
+video_file = str(sys.argv[2]) + ".avi"
 
 # Create holding array
 img_array = []
 img_array = sorted(glob.iglob(img_folder), key = os.path.getctime, reverse = False)
-
+img_out_array = []
+#print(img_array)
 print("Start creating video ... " + img_folder)
 
 # Loop over all files in our bg folder
@@ -34,24 +36,27 @@ for x in range(0, len(img_array)):
     # read image and size for generating video
     # TODO size we need only once because all are the same size but doesnt matter i think
     img = cv.imread(img_array[x])
-    print(img)
+    #print(img)
     height, width, layers = img.shape
     size = (width, height)
     # add to array
-    img_array.append(img)
+    img_out_array.append(img)
 
 print("All files inside our array, total length " + str(len(img_array)))
+
 
 # create our video container
 out = cv.VideoWriter(video_file, cv.VideoWriter_fourcc(*'DIVX'), 15, size)
 
 print("Video container created")
 
-for i in range(0, len(img_array)):
-    print("Write image to video " + str(i))
-    out.write(img_array[i])
+counter = 0
+for i in img_out_array:
+    print("Write image to video " + str(counter))
+    out.write(i)
+    counter += 1
 
-print("Finsihed, releasing file")
+print("Finished, releasing file")
 out.release()
 
 # END
