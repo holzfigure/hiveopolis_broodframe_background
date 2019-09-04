0947564360ae14f3af69a89689c4845392a7e861
Hannes Oberreiter <office@btree.at>
2019-09-04 17:25:03 +0200

    foreground logic without pybgs

---
+++ foreground_raw.py
@@ -0,0 +1,212 @@
+####
+# RAW file does not rely on pybgs library and only needs opencv
+
+########################################
+########################################
+# This sript will take video.MP4 exract
+# a given area and calculate the pixel difference
+# between each frame after n frames it
+# takes the mean and plot it
+# settings for pixel difference in
+# /config/FrameDifference.xml
+# SETTINGS:
+# you can jump frames forward to see variable jump_frames
+# if you set save_plot the plots will get saved into plot folder
+########################################
+########################################
+
+# Basic libraries
+import numpy as np
+import os
+import glob
+
+# open cv, we have to load V3 for full list of algorithms
+# https://docs.opencv.org/3.4
+import cv2 as cv
+# timer are used for stats, too compare running time of algorithms
+import time
+
+# graphic
+import matplotlib.pyplot as plt
+plt.style.use('fast')
+
+# https://makersportal.com/blog/2018/8/14/real-time-graphing-in-python
+def live_plotter(x_vec, y1_data, line1, ax, frame, save, pause_time = 0.04):
+
+    if line1 == []:
+        # this is the call to matplotlib that allows dynamic plotting
+        plt.ion()
+        fig = plt.figure(figsize = (8 , 4))
+        ax = fig.add_subplot(1,1,1)
+        # create a variable for the line so we can later update it
+        line1, = ax.plot(x_vec, y1_data, '-o', alpha = 0.8)
+        #update plot label/title
+        plt.ylabel('Moved pixels [%]')
+        plt.xlabel('Frames [5s Interval]')
+        plt.title('Pixel change')
+        plt.ylim(0,15,0.5)
+        plt.show()
+
+    # change color if we drop lower than given threshold
+    if(y1_data[-1] < 2):
+        ax.set_facecolor('xkcd:salmon')
+    else:
+        ax.set_facecolor('white')
+
+    # after the figure, axis, and line are created, we only need to update the y-data
+    line1.set_ydata(y1_data)
+    line1.set_xdata(x_vec)
+
+    plt.xlim(min(x_vec),max(x_vec), 5)
+
+    plt.tick_params(
+        axis = 'x',          # changes apply to the x-axis
+        which = 'both',      # both major and minor ticks are affected
+        bottom = True,      # ticks along the bottom edge are off
+        top = False,         # ticks along the top edge are off
+        labelbottom = True) # labels along the bottom edge are off
+
+    # adjust limits if new data goes beyond bounds
+    if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
+       plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
+    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
+    plt.pause(pause_time)
+
+    # if we want to save the plots
+    if save:
+        fname = 'plot/'+str(frame)+'.png'
+        plt.savefig(fname, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', papertype=None, format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)    # return line so we can update it again in the next iteration
+    return line1, ax
+
+# our Video file
+video_file = "video.MP4"
+# jump frames forward
+jump_frames = 0
+# save plot
+save_plot = False
+# threshold
+treshold = 50
+
+# helper arrays
+w_pixel_array = []
+ax = []
+x_vec = []
+y_vec = []
+
+second_count = 0        # used to count frames
+n_white_pix_sum = 0     # helper variable to sum white pixel in n amount of frames
+
+# create folder for plots and empty it
+folder = "plot"
+# create output folder if not exists
+try:
+    os.mkdir(folder)
+except:
+    print("{} Folder already exists".format(folder))
+    i = input("Clear files in {} folder (y/n)?".format(folder))
+    if (i == "y"):
+        # empty output folder
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+        print("Files deleted")
+
+# load video
+capture = cv.VideoCapture(video_file)
+# wait till video is loaded
+while not capture.isOpened():
+    capture = cv.VideoCapture(video_file)
+    cv.waitKey(1000)
+    print("Wait for the header")
+
+# jump forward in frames
+capture.set(cv.CAP_PROP_POS_FRAMES, jump_frames)
+
+img_history = []
+
+while True:
+    # read video
+    flag, frame = capture.read()
+
+    # check if video is read
+    if flag:
+        # show video in ouput
+        cv.namedWindow('original',cv.WINDOW_NORMAL)
+        cv.resizeWindow('original', 600, 400)
+        # draws a rectangle on frame
+        cv.rectangle(frame, (700, 200), (1730, 750), (255,0,0), 2)
+        cv.imshow('original', frame)
+
+        # crop only the area of interest for us
+        roi = frame[200:750+1, 700:1730+1]
+        cv.namedWindow('cropped',cv.WINDOW_NORMAL)
+        cv.resizeWindow('cropped', 600, 400)
+        cv.imshow('cropped', roi)
+
+        pos_frame = capture.get(1)
+
+        # check if it was the first run then we use same picture for history as input
+        if (len(img_history) == 0):
+            img_history = roi
+
+        # calculate absdiff
+        img_output = cv.absdiff(roi, img_history)
+        # change image to grayscale
+        img_output = cv.cvtColor(img_output, cv.COLOR_BGR2GRAY);
+        # maybe add Gaussian?
+        img_output = cv.GaussianBlur(img_output, (3, 3), 0)
+
+        # exports a black and white image
+        (thresh, img_output) = cv.threshold(img_output, treshold, 255, cv.THRESH_BINARY)
+
+        # show foreground mask in window
+        cv.namedWindow('foreground', cv.WINDOW_NORMAL)
+        cv.resizeWindow('foreground', 600, 400)
+        cv.imshow('foreground', img_output)
+
+        # all pixels of image
+        # TODO actually we would need this only one time, as all the frames have the same pixel count
+        n_all_px = img_output.size
+        # get all white pixels == changed pixels
+        n_white_pix = np.sum(img_output == 255)
+        # save into our helper variable
+        n_white_pix_sum = n_white_pix_sum + n_white_pix
+        # set our frame counter forward
+        second_count = second_count + 1
+
+        # if 10 frames we output the plot
+        if (second_count == 5):
+            # mean and relative value to all pixels of the cropped frame
+            relative_white = (n_white_pix_sum / second_count) /  n_all_px * 100
+            # add value our vector
+            y_vec.extend([relative_white])
+            x_vec.extend([pos_frame])
+
+            # create our live plot
+            w_pixel_array, ax = live_plotter(x_vec, y_vec, w_pixel_array, ax, pos_frame, save_plot)
+
+            # move our vector forward
+            if (len(x_vec) > 500):
+                y_vec.pop(0)
+                x_vec.pop(0)
+
+            # reset helper
+            n_white_pix_sum = 0
+            second_count = 0
+
+            print('Number of mean white pixels:', relative_white)
+
+    else:
+        #print "Frame is not ready"
+        cv.waitKey(1000)
+        break
+
+    if 0xFF & cv.waitKey(10) == 27:
+        break
+
+# END
