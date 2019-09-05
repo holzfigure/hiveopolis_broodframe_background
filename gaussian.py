f186171485e1e3d8620128c15ab448536d773c50
Hannes Oberreiter <office@btree.at>
2019-09-05 08:17:54 +0200

    cleaned up and added possibility to use a preprocessing of the images

--- gaussian.py
+++ gaussian.py
@@ -1,3 +1,10 @@
+########################################
+########################################
+# This script removes bees from brood frames
+# of a series of pictures with the Gaussian Logic
+########################################
+########################################
+
 # Basic libraries
 import numpy as np
 import os
@@ -6,36 +13,48 @@ import glob
 # https://docs.opencv.org/3.4
 import cv2 as cv
 
-
+############## SETTINGS #######################
 # path to raw files and output folder of generated images
 path_raw = 'img'
 path_output = 'out'
+# https://docs.opencv.org/master/d7/df6/classcv_1_1BackgroundSubtractor.html#aa735e76f7069b3fa9c3f32395f9ccd21
+learning_rate = -1
+# max runs if we want to limit generation, False or 0 for no max runs
+max_runs = 0
+# print modulus, only used for output of text
+print_modulus = 10
+#### MOG Settings
+history = 200
+shadow = False
+VarThreshold = 16
+############## END SETTINGS #######################
+
+print("###########  SETTINGS  ##################")
+print("###########  Path raw: {}".format(path_raw))
+print("###########  Path output: {}".format(path_output))
+print("###########  Print modulus: {}".format(print_modulus))
+print("###########  Max runs: {}".format(max_runs))
+print("###########  Learning rate: {}".format(learning_rate))
+print("###########  History: {}".format(history))
+print("###########  Shadow: {}".format(shadow))
+print("###########  VarThreshold: {}".format(VarThreshold))
 
 # load CV BackgroundSubtractorMOG2 with settings
 # https://docs.opencv.org/master/d6/d17/group__cudabgsegm.html
 # attribues found inside Class
 # 'setBackgroundRatio', 'setComplexityReductionThreshold', 'setDetectShadows', 'setHistory', 'setNMixtures', 'setShadowThreshold', 'setShadowValue', 'setVarInit', 'setVarMax', 'setVarMin', 'setVarThreshold', 'setVarThresholdGen'
 mog = cv.createBackgroundSubtractorMOG2();
-mog.setDetectShadows(False);
-mog.setHistory(200);
-mog.setVarThreshold(16)
+mog.setDetectShadows(shadow);
+mog.setHistory(history);
+mog.setVarThreshold(VarThreshold)
 
-# https://docs.opencv.org/master/d7/df6/classcv_1_1BackgroundSubtractor.html#aa735e76f7069b3fa9c3f32395f9ccd21
-learning_rate = -1
-
-# threshold how to build it into the code?
-#threshold = 0.05
-#cv.threshold(img_foreground, img_foreground, threshold, 255, cv.THRESH_BINARY);
-
-# max runs if we want to limit generation
-max_runs = 500000
 
 if(os.path.isdir(path_raw) == False):
     print("####### Missng {} folder ###########".format(path_raw))
     exit()
 
 # load all img as array reverse sorted with oldest at beginning
-array = sorted(glob.iglob(path_raw + '/*.jpg'), key = os.path.getctime, reverse = True)
+array = sorted(glob.iglob(path_raw + '/*.jpg'), key = os.path.getmtime, reverse = True)
 
 # create output folder if not exists
 try:
@@ -55,7 +74,9 @@ except:
                 print(e)
         print("Files deleted")
 
+print("#############################")
 print("Starting with total file number: " + str(len(array)))
+print("#############################")
 
 # loop x times as files in our folder
 for x in range(0, len(array)):
@@ -63,23 +84,38 @@ for x in range(0, len(array)):
     # we can loop now through our array of images
     img_path = array[x]
 
-    print("Load image: ", img_path)
-
     # read file into open cv and apply to algorithm to generate background model
     img = cv.imread(img_path)
+
+    #### Preprocessing ######
+    # change image to grayscale
+    #img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # equalize grayscale image
+    #img = cv.equalizeHist(img)
+    # add gaussian to remove noise
+    #img = cv.GaussianBlur(img, (1, 1), 0)
+    #img = cv.medianBlur(img, 1)
+    #img = cv.GaussianBlur(img, (7, 7), 1.5)
+    #### END Preprocessing ######
+
     img_output = mog.apply(img, learning_rate);
+
+    # threshold for foreground mask, we don't use the foreground mask so we dont need it?
+    #img_output = cv.threshold(img_output, 10, 255, cv.THRESH_BINARY);
+
+    # get background image
     img_bgmodel = mog.getBackgroundImage();
 
-    # wite into algorithm folder the finished backgroundModels
+    # wite finished backgroundModels
     img_bg = img_path.replace(path_raw, path_output)
-    # print(img_bg)
     cv.imwrite(img_bg, img_bgmodel)
-    # if ends if TODO included
 
-    if (x == max_runs):
-        break
+    # break if max runs is defined and reached
+    if(max_runs > 0):
+        if (x == max_runs):
+            break
 
-    print("Runs left: " + str(len(array)-x))
-
+    if (x % print_modulus == 0):
+        print("Load image: ", img_path + "\n Runs left: " + str(len(array)-x))
 
 # END
