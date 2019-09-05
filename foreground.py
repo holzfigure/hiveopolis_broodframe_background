71233ca08e24653a39414d409b49766bc2800213
Hannes Oberreiter <office@btree.at>
2019-09-05 07:59:58 +0200

    renamed files more logical and improved foreground logic

--- foreground.py
+++ foreground.py
@@ -1,11 +1,13 @@
 ########################################
+# this file does not use pybgs library
+########################################
+
+########################################
 ########################################
 # This sript will take video.MP4 exract
 # a given area and calculate the pixel difference
 # between each frame after n frames it
 # takes the mean and plot it
-# settings for pixel difference in
-# /config/FrameDifference.xml
 # SETTINGS:
 # you can jump frames forward to see variable jump_frames
 # if you set save_plot the plots will get saved into plot folder
@@ -16,23 +18,93 @@
 import numpy as np
 import os
 import glob
-
 # open cv, we have to load V3 for full list of algorithms
 # https://docs.opencv.org/3.4
 import cv2 as cv
-# our algorithm library please refer and citate https://github.com/andrewssobral/bgslibrary
-# c++ but with python wrapper, has multiple algorithms which are not standard included in open cv
-# does background image generation with the foremask from cv2
-import pybgs as bgs
-# timer are used for stats, too compare running time of algorithms
-import time
-
 # graphic
 import matplotlib.pyplot as plt
+
+############## SETTINGS #######################
+# plot style
 plt.style.use('fast')
+# our Video file
+video_file = "video.MP4"
+# create folder for plots and empty it
+folder = "plot"
+# jump frames forward
+jump_frames = 6000
+# save plot
+save_plot = False
+# threshold
+threshold = 7
+# our rectangle area
+x1 = 1130
+x2 = 1800
+y1 = 130
+y2 = 900
+# plot pixel color change break point
+plot_breakpoint = 6
+############## END SETTINGS #######################
+
+# helper variables
+w_pixel_array = []
+ax = []
+x_vec = []
+y_vec = []
+second_count = 0        # used to count frames
+n_white_pix_sum = 0     # helper variable to sum white pixel in n amount of frames
+
+print("###########  SETTINGS  ##################")
+
+print("###########  PLOTS: {}".format(save_plot))
+print("###########  PLOT breakpoint: {}".format(plot_breakpoint))
+print("###########  Ignored frames: {}".format(jump_frames))
+print("###########  Threshold: {}".format(threshold))
+print("###########  Video: {}".format(video_file))
+print("###########  X1: {}".format(x1))
+print("###########  X2: {}".format(x2))
+print("###########  Y1: {}".format(y1))
+print("###########  Y2: {}".format(y2))
+
+# Check if video file exists
+if(os.path.isfile(video_file) == False):
+    print("Error: No video file found")
+    exit()
+
+# create output folder if not exists
+try:
+    os.mkdir(folder)
+except:
+    print("{} Folder already exists".format(folder))
+    i = input("Clear files in {} folder (y/n)?".format(folder))
+    if (i == "y"):
+        # empty output folder
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+        print("Files deleted")
+
+# load video
+capture = cv.VideoCapture(video_file)
+# wait till video is loaded
+while not capture.isOpened():
+    capture = cv.VideoCapture(video_file)
+    cv.waitKey(1000)
+    print("Wait for the header")
+
+# jump forward in frames
+capture.set(cv.CAP_PROP_POS_FRAMES, jump_frames)
+
+length = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+print( "Video total frames: {}".format(length) )
 
 # https://makersportal.com/blog/2018/8/14/real-time-graphing-in-python
-def live_plotter(x_vec, y1_data, line1, ax, frame, save, pause_time = 0.04):
+def live_plotter(x_vec, y1_data, line1, ax, frame, save, plot_breakpoint, pause_time = 0.04):
 
     if line1 == []:
         # this is the call to matplotlib that allows dynamic plotting
@@ -49,7 +121,7 @@ def live_plotter(x_vec, y1_data, line1, ax, frame, save, pause_time = 0.04):
         plt.show()
 
     # change color if we drop lower than given threshold
-    if(y1_data[-1] < 2):
+    if(y1_data[-1] < plot_breakpoint):
         ax.set_facecolor('xkcd:salmon')
     else:
         ax.set_facecolor('white')
@@ -68,8 +140,8 @@ def live_plotter(x_vec, y1_data, line1, ax, frame, save, pause_time = 0.04):
         labelbottom = True) # labels along the bottom edge are off
 
     # adjust limits if new data goes beyond bounds
-    #if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
-    #   plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
+    if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
+       plt.ylim([np.min(0),np.max(y1_data)+np.std(y1_data)])
     # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
     plt.pause(pause_time)
 
@@ -78,52 +150,6 @@ def live_plotter(x_vec, y1_data, line1, ax, frame, save, pause_time = 0.04):
         fname = 'plot/'+str(frame)+'.png'
         plt.savefig(fname, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', papertype=None, format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)    # return line so we can update it again in the next iteration
     return line1, ax
-
-# Our used algorithm
-algorithm = bgs.FrameDifference()
-# our Video file
-video_file = "video.MP4"
-# jump frames forward
-jump_frames = 0
-# save plot
-save_plot = False
-
-# helper arrays
-w_pixel_array = []
-ax = []
-x_vec = []
-y_vec = []
-
-second_count = 0        # used to count frames
-n_white_pix_sum = 0     # helper variable to sum white pixel in n amount of frames
-
-# create folder for plots and empty it
-folder = "plot"
-try:
-    os.mkdir(folder)
-except:
-    print("Folder is already created")
-
-for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-    except Exception as e:
-        print(e)
-
-
-# load video
-capture = cv.VideoCapture(video_file)
-# wait till video is loaded
-while not capture.isOpened():
-    capture = cv.VideoCapture(video_file)
-    cv.waitKey(1000)
-    print("Wait for the header")
-
-# jump forward in frames
-capture.set(cv.CAP_PROP_POS_FRAMES, jump_frames)
 
 while True:
     # read video
@@ -135,22 +161,59 @@ while True:
         cv.namedWindow('original',cv.WINDOW_NORMAL)
         cv.resizeWindow('original', 600, 400)
         # draws a rectangle on frame
-        cv.rectangle(frame, (700, 200), (1730, 750), (255,0,0), 2)
+
+        # x1: y1, x2: y2
+        cv.rectangle(frame, (x1, y1), (x2, y2), (255,0,0), 2)
         cv.imshow('original', frame)
 
         # crop only the area of interest for us
-        roi = frame[200:750+1, 700:1730+1]
+        # y1:y2, x1:x2
+        roi = frame[y1:y2+1, x1:x2+1]
         cv.namedWindow('cropped',cv.WINDOW_NORMAL)
         cv.resizeWindow('cropped', 600, 400)
         cv.imshow('cropped', roi)
 
-
-        # TODO change X-Axis labels to frame number?
         pos_frame = capture.get(1)
-        # apply given algroithm to get foregorund mask
-        img_output = algorithm.apply(roi)
+
+        #### Preprocessing ######
+        # change image to grayscale
+        roi = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
+        # equalize grayscale image
+        #roi = cv.equalizeHist(roi)
+        # add gaussian to remove noise
+        #roi = cv.GaussianBlur(roi, (1, 1), 0)
+        #roi = cv.medianBlur(roi, 1)
+        #roi = cv.GaussianBlur(roi, (7, 7), 1.5)
+        #### END Preprocessing ######
+
+        cv.namedWindow('preprocess', cv.WINDOW_NORMAL)
+        cv.resizeWindow('preprocess', 600, 400)
+        cv.imshow('preprocess', roi)
+
+        # check if it was the first run otherwise img_history is same as input for first round
+        try:
+            img_history
+        except:
+            img_history = roi
+
+        # calculate absdiff
+        img_output = cv.absdiff(roi, img_history)
+
+        #### Output Processing ######
+        #img_output = cv.cvtColor(img_output, cv.COLOR_BGR2GRAY)
+        #img_output = cv.equalizeHist(img_output)
+        #img_output = cv.GaussianBlur(img_output, (7, 7), 1.5)
+        #img_output = cv.GaussianBlur(img_output, (3, 3), 1.5)
+        img_output = cv.medianBlur(img_output, 1)
+
+
+        # exports a black and white image
+        _, img_output = cv.threshold(img_output, threshold, 255, cv.THRESH_BINARY)
+
+        img_history = roi
+
         # show foreground mask in window
-        cv.namedWindow('foreground',cv.WINDOW_NORMAL)
+        cv.namedWindow('foreground', cv.WINDOW_NORMAL)
         cv.resizeWindow('foreground', 600, 400)
         cv.imshow('foreground', img_output)
 
@@ -173,7 +236,7 @@ while True:
             x_vec.extend([pos_frame])
 
             # create our live plot
-            w_pixel_array, ax = live_plotter(x_vec, y_vec, w_pixel_array, ax, pos_frame, save_plot)
+            w_pixel_array, ax = live_plotter(x_vec, y_vec, w_pixel_array, ax, pos_frame, save_plot, plot_breakpoint)
 
             # move our vector forward
             if (len(x_vec) > 500):
