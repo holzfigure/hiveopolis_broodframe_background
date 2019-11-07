1c96994368db30eb300224a18a567403b9a6067f
holzfigure <daniel.hofstadler@uni-graz.at>
2019-11-08 00:07:40 +0100

    (temporarily?) remove the opencv backgrounddetection parts

--- get_backgrounds.py
+++ get_backgrounds.py
@@ -33,7 +33,7 @@ import pandas as pd
 import numpy as np
 # open cv, we have to load V3 for full list of algorithms
 # https://docs.opencv.org/3.4
-import cv2 as cv
+# import cv2 as cv
 
 import iohelp as ioh
 
@@ -218,32 +218,32 @@ def initialize_io(dir_in=PATH_RAW, dir_out=PATH_OUT,
     logging.info(f"numpy version: {np.__version__}")
     logging.info(f"pandas version: {pd.__version__}")
     # logging.info(f"networkx version: {nx.__version__}")
-    logging.info(f"OpenCV version: {cv.__version__}")
+    # logging.info(f"OpenCV version: {cv.__version__}")
 
     return dir_in, dir_out
 
 
-def adjust_gamma(image, ltable):
-    """Apply gamma correction using the lookup table."""
-    return cv.LUT(image, ltable)
-
-
-def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0,
-                 amount=1.0, threshold=0):
-    """Return a sharpened version of the image, using an unsharp mask.
-
-    https://en.wikipedia.org/wiki/Unsharp_masking
-    https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm"""
-    blurred = cv.GaussianBlur(image, kernel_size, sigma)
-    sharpened = float(amount + 1) * image - float(amount) * blurred
-    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
-    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
-    sharpened = sharpened.round().astype(np.uint8)
-    if threshold > 0:
-        low_contrast_mask = np.absolute(image - blurred) < threshold
-        # OpenCV4 function copyTo
-        np.copyto(sharpened, image, where=low_contrast_mask)
-    return sharpened
+# def adjust_gamma(image, ltable):
+#     """Apply gamma correction using the lookup table."""
+#     return cv.LUT(image, ltable)
+#
+#
+# def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0,
+#                  amount=1.0, threshold=0):
+#     """Return a sharpened version of the image, using an unsharp mask.
+#
+#     https://en.wikipedia.org/wiki/Unsharp_masking
+#     https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm"""
+#     blurred = cv.GaussianBlur(image, kernel_size, sigma)
+#     sharpened = float(amount + 1) * image - float(amount) * blurred
+#     sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+#     sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+#     sharpened = sharpened.round().astype(np.uint8)
+#     if threshold > 0:
+#         low_contrast_mask = np.absolute(image - blurred) < threshold
+#         # OpenCV4 function copyTo
+#         np.copyto(sharpened, image, where=low_contrast_mask)
+#     return sharpened
 
 
 def folder_datetime(foldername, time_infolder_fmt=TIME_INFOLDER_FMT):
